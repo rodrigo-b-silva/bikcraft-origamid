58647ade129e8cf1a3f87cd8d19a1df6ccf92589
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-08-29 00:30:49 -0300

    finalização do projeto

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,33 @@
-$(".rslides").responsiveSlides({
-  auto: true,             // Boolean: Animate automatically, true or false
-  speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-});
+$(document).ready(function() {
+  $(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  });
+  
+  
+  Visibility.onVisible(function () {
+    setTimeout(function() {
+      $(".introducao h1").addClass("animate__animated animate__fadeInDown")
+    }, 300);
+    setTimeout(function() {
+      $(".introducao blockquote").addClass("animate__animated animate__fadeInDown")
+    }, 600);
+    setTimeout(function() {
+      $(".introducao .btn").addClass("animate__animated animate__fadeInDown")
+    }, 900);
+    setTimeout(function() {
+      $(".animar").addClass("animate__animated animate__fadeInDown")
+    }, 1200);
+
+    setTimeout(function() {
+      $(".introducao-interna h1").addClass("animate__animated animate__fadeInDown")
+    }, 300);
+    setTimeout(function() {
+      $(".introducao-interna p").addClass("animate__animated animate__fadeInDown")
+    }, 600);
+    setTimeout(function() {
+      $(".animar-interno").addClass("animate__animated animate__fadeInDown")
+    }, 900);
+  });
+})
